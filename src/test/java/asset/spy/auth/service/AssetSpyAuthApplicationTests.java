652a27151a0ff43066c76ac3132cee2df84c3bf4
Victoria Shilova <vvsslova@mail.ru>
2025-03-10 16:40:50 +0300

    Created Spring Boot frame for auth service

---
+++ src/test/java/asset/spy/auth/service/AssetSpyAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package asset.spy.auth.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetSpyAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
